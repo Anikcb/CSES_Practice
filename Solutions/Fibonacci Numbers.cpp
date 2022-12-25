007f9381606c07a639475c1ee740a7bb7f0b0936
Anik chakraborty <anikchakraborty562@gmail.com>
2022-12-25 23:43:12 +0600

    Create Fibonacci Numbers.cpp

---
+++ Solutions/Fibonacci Numbers.cpp
@@ -0,0 +1,120 @@
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+#include<bits/stdc++.h>
+
+//using    namespace __gnu_pbds;
+using namespace std;
+
+
+/*** Typedef ***/
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+/*** STLs ***/
+typedef vector <ll> vll;
+typedef set <ll> sll;
+typedef multiset <ll> msll;
+typedef queue <ll> qll;
+typedef stack <ll> stll;
+typedef map <ll, ll> mll;
+typedef pair <ll, ll> pll;
+typedef vector <pair <ll , ll> > vpll;
+typedef map<string,ll> msl;
+typedef priority_queue<ll> pql;
+typedef priority_queue<ll,vector<ll>,greater<ll> > npql;
+
+
+/*** Values ***/
+const int N = 100005;
+const int MOD = 1000000007;
+const int inf = 1234567890;
+const ll INF = 1122334455667788990;
+
+
+/*** Define Values ***/
+#define     F                 first
+#define     S                 second
+#define     re                return
+#define     MP                make_pair
+#define     pb                push_back
+#define     SZ(x)             ((ll) (x).size())
+#define     EPS               10E-10
+#define     iseq(a,b)         (fabs(a-b)<EPS)
+#define     PI                3.141592653589793238462643
+#define     HAR               0.57721566490153286060651209
+#define     Ceil(a,b)         (a+b-1)/b
+#define     gcd(a, b)         __gcd(a,b)
+#define     lcm(a, b)         ((a)/gcd(a,b))*(b)
+#define     input             freopen("input.txt","rt", stdin)
+#define     output            freopen("output.txt","wt", stdout)
+#define     all(v)            v.begin(),v.end()
+#define     en                cout << '\n';
+#define     no                cout << "NO" << '\n'
+#define     yes               cout << "YES" << '\n'
+#define     memz(u)           memset(u, 0, sizeof u)
+#define     memn(u)           memset(u, -1, sizeof u)
+#define     ps(x,y)           fixed<<setprecision(y)<<x
+#define     foR0(num)         for(ll i = 0; i < num; i++)
+#define     foR1(num)         for(ll i = 1; i <= num; i++)
+#define     rep(i, x, n)      for (ll i = x, _n = (n); i < _n; ++i)
+#define     forIn(arr, num)   for(ll i = 0; i < num; i++) cin>>arr[i];
+#define     forIn1(arr, num)  for(ll i = 1; i <= num; i++) cin>>arr[i];
+#define     Unique(X)         (X).resize(unique(all(X))-(X).begin())
+#define     IOS               ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+
+//__uint128_t
+ll A[N*10];
+ll B[N*10];
+
+
+
+void multiply(ll F[2][2], ll M[2][2])
+{
+    ll x =  ((F[0][0] * M[0][0])%MOD + (F[0][1] * M[1][0])%MOD )%MOD ;
+    ll y =  ((F[0][0] * M[0][1])%MOD  + (F[0][1] * M[1][1])%MOD )%MOD ;
+    ll z =  ((F[1][0] * M[0][0])%MOD  + (F[1][1] * M[1][0])%MOD )%MOD ;
+    ll w =  ((F[1][0] * M[0][1])%MOD  + (F[1][1] * M[1][1])%MOD )%MOD ;
+    F[0][0] = x;
+    F[0][1] = y;
+    F[1][0] = z;
+    F[1][1] = w;
+}
+
+void power(ll F[2][2], ll n)
+{
+    if (n == 0 || n == 1)return;
+    ll M[2][2] = {{1,1},{1,0}};
+    power(F, n / 2);
+    multiply(F, F);
+    if (n&1)multiply(F, M);
+}
+
+ll fibo_matrix(ll n)
+{
+    ll F[2][2] = {{1,1},{1,0}};
+    if (n == 0)return 0;
+    power(F, n - 1);
+    return F[0][0];
+}
+
+
+
+int main()
+{
+    IOS;
+    ll tst=1;
+    //cin>>tst;
+    for(ll tt=1; tt<=tst; tt++)
+    {
+        //code
+        ll n;
+        cin>>n;
+        cout<<fibo_matrix(n)<<endl;
+
+    }
+
+
+    return 0;
+}
